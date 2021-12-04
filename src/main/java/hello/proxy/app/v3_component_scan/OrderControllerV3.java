365d0b7b9990b2db160ddff5dcf9813180eb0bf2
jakeseo <n00nietzsche@gmail.com>
2021-12-04 15:00:16 +0900

    스프링 핵심 원리 - 고급편 #32 프록시 패턴과 데코레이터 패턴 - 예제 프로젝트 만들기 v3

---
+++ src/main/java/hello/proxy/app/v3_component_scan/OrderControllerV3.java
@@ -0,0 +1,29 @@
+package hello.proxy.app.v3_component_scan;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Slf4j
+@Controller // 컴포넌트 스캔의 대상이 되지 않기 위해
+@ResponseBody
+public class OrderControllerV3 {
+    private final OrderServiceV3 orderService;
+
+    public OrderControllerV3(OrderServiceV3 orderService) {
+        this.orderService = orderService;
+    }
+
+    @GetMapping("/v3/request")
+    public String request(String itemId) {
+        orderService.orderItem(itemId);
+        return "ok";
+    }
+
+    @GetMapping("/v3/no-log")
+    public String noLog() {
+        return "ok";
+    }
+}
---
+++ src/main/java/hello/proxy/app/v3_component_scan/OrderServiceV3.java
@@ -0,0 +1,16 @@
+package hello.proxy.app.v3_component_scan;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderServiceV3 {
+    private final OrderRepositoryV3 orderRepository;
+
+    public OrderServiceV3(OrderRepositoryV3 orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
+    public void orderItem(String itemId) {
+        orderRepository.save(itemId);
+    }
+}
