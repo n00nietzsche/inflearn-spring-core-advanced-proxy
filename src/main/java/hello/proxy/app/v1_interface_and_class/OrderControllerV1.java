98894c36bad476b07f2ea5db72aacab06eb607a3
jakeseo <n00nietzsche@gmail.com>
2021-12-04 13:34:56 +0900

    스프링 핵심 원리 - 고급편 #30 프록시 패턴과 데코레이터 패턴 - 예제 프로젝트 만들기 v1

--- src/main/java/hello/proxy/app/v1_interface_and_class/OrderControllerV1.java
+++ src/main/java/hello/proxy/app/v1_interface_and_class/OrderControllerV1.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public interface OrderControllerV1 {
     @GetMapping("/v1/request")
     String request(@RequestParam("itemId") String itemId);
+
     @GetMapping("/v1/no-log")
     String noLog();
 }
--- src/main/java/hello/proxy/app/v1_interface_and_class/OrderControllerV1Impl.java
+++ src/main/java/hello/proxy/app/v1_interface_and_class/OrderControllerV1Impl.java
@@ -1,4 +1,20 @@
 package hello.proxy.app.v1_interface_and_class;
 
-public class OrderControllerV1Impl {
+public class OrderControllerV1Impl implements OrderControllerV1 {
+    private final OrderServiceV1 orderService;
+
+    public OrderControllerV1Impl(OrderServiceV1 orderService) {
+        this.orderService = orderService;
+    }
+
+    @Override
+    public String request(String itemId) {
+        orderService.orderItem(itemId);
+        return "ok";
+    }
+
+    @Override
+    public String noLog() {
+        return "ok";
+    }
 }
