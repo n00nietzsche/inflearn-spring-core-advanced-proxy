758f370a195abe70333f691d47823c8fc94ec551
jakeseo <n00nietzsche@gmail.com>
2021-12-05 15:41:52 +0900

    스프링 핵심 원리 - 고급편 #43 구체 클래스 기반 프록시 - 예제2

--- src/test/java/hello/proxy/pure_proxy/concrete_proxy/ConcreteProxyTest.java
+++ src/test/java/hello/proxy/pure_proxy/concrete_proxy/ConcreteProxyTest.java
@@ -2,6 +2,7 @@ package hello.proxy.pure_proxy.concrete_proxy;
 
 import hello.proxy.pure_proxy.concrete_proxy.code.ConcreteClient;
 import hello.proxy.pure_proxy.concrete_proxy.code.ConcreteLogic;
+import hello.proxy.pure_proxy.concrete_proxy.code.TimeProxy;
 import org.junit.jupiter.api.Test;
 
 public class ConcreteProxyTest {
@@ -11,5 +12,14 @@ public class ConcreteProxyTest {
         ConcreteLogic concreteLogic = new ConcreteLogic();
         ConcreteClient client = new ConcreteClient(concreteLogic);
         client.execute();
+    }
+
+    @Test
+    void addProxy() {
+        ConcreteLogic concreteLogic = new ConcreteLogic();
+        TimeProxy timeProxy = new TimeProxy(concreteLogic);
+        ConcreteClient concreteClient = new ConcreteClient(timeProxy);
+
+        concreteClient.execute();
     }
 }
